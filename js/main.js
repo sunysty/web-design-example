0329e2db0bbe91f72dd6b55d0999edc858382653
sunysty <seokjiseon@gmail.com>
2021-06-19 17:38:19 +0900

    rotation 추가

--- js/main.js
+++ js/main.js
@@ -7,4 +7,24 @@ for(let el of lists){
     el.style.transform = `rotate(${45*i}deg) translateY(-100vh)`;
     el.querySelector(".pic").style.backgroundImage = `url(img/member${i+1}.jpg)`;
     i++;
+
+    let play = el.querySelector(".play");
+    let pause = el.querySelector(".pause");
+    let load = el.querySelector(".load");
+
+    play.addEventListener("click", e =>{
+        e.currentTarget.parentElement.nextElementSibling.play();
+        e.currentTarget.parentElement.parentElement.previousElementSibling.classList.add("on");
+    });
+
+    pause.addEventListener("click", e =>{
+        e.currentTarget.parentElement.nextElementSibling.pause();
+        e.currentTarget.parentElement.parentElement.previousElementSibling.classList.remove("on");
+    });
+
+    load.addEventListener("click", e=>{
+        e.currentTarget.parentElement.nextElementSibling.load();
+        e.currentTarget.parentElement.nextElementSibling.remove();
+        e.currentTarget.parentElement.parentElement.previousElementSibling.classList.add("on");
+    });
 }
